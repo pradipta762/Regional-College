34e0c9f8d0e2e54a771e35fca53230c99fe631b7
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2024-02-15 21:26:43 +0530

    Design Changes

--- js/script.js
+++ js/script.js
@@ -28,3 +28,61 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+//Enquiry Form Section
+const formOpenBtn = document.querySelector("#form-open");
+const formCloseBtn = document.querySelector("#form-close");
+const popupForm = document.querySelector("#popupForm");
+
+// When the user click the button, show the popup
+formOpenBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    popupForm.style.display = "block";
+});
+
+// When the user clicks the close button or outside the form, close the form window
+popupForm.addEventListener("click", () => {
+    if(event.target.classList.contains("close") || event.target == popupForm ) {
+        popupForm.style.display = "none";
+    }
+});
+
+
+
+// Counter Section
+function animateCounters() {
+    const counters = document.querySelectorAll('.count');
+    const speed = 2000; // Adjust animation speed (lower is faster)
+
+    counters.forEach(counter => {
+        const target = +counter.innerText;
+        const updateCount = () => {
+            const count = +counter.innerText;
+            const inc = target / speed;
+            if (count < target) {
+                counter.innerText = Math.ceil(count + inc);
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCount();
+    });
+}
+
+// Trigger counter animation when element is in viewport
+document.addEventListener('DOMContentLoaded', () => {
+    const counterSection = document.querySelector('.counter-section');
+    const options = {
+        threshold: 0.5 // Change threshold as needed
+    };
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                animateCounters();
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+    observer.observe(counterSection);
+});
