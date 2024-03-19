9e5e93dee2d1fb3224557923fd97292aef550ef3
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2024-03-19 08:08:10 +0530

    (Updated) : Navbar Updated & Department section added

--- js/script.js
+++ js/script.js
@@ -9,6 +9,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
+});
+
 
 // Slider Section
 
