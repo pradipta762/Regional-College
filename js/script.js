8d7dd661d23e08f634156a363bae6648bb6e60a2
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2024-05-08 20:05:05 +0530

    (Update) : Preloader Suspended

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Preloader Section
-let loader = document.querySelector(".preloader");
-    window.addEventListener("load", () => {
-    loader.style.display = "none";
-})
+// let loader = document.querySelector(".preloader");
+//     window.addEventListener("load", () => {
+//     loader.style.display = "none";
+// })
 
 // Navigation Section
 
